64feb6ec4ce302aa1a8540e66ac5c4da9a2c53a0
nickkimbrough <26373139+nickkimbrough@users.noreply.github.com>
2025-12-09 09:51:13 -0700

    Solve Day 8

--- AdventOfCode2025/Day8/Day8.cs
+++ AdventOfCode2025/Day8/Day8.cs
@@ -2,7 +2,7 @@
 
 public class Day8
 {
-    public static int Day8Part1Answer()
+    public static int Day8Part1Answer(int connectionCount = 1000)
     {
         /** Initial Thoughts:
          *  Well, goodbye 2d, hello 3d! There are 1000 junction boxes
@@ -29,14 +29,18 @@ public class Day8
 
         // Take in the input
         var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Day8", "input.txt");
-        List<Junction> junctions = [.. File.ReadAllLines(filePath).Select(x =>
+        var junctionArrays = File.ReadAllLines(filePath).Select(x =>
             {
-                var splitOut = x
+                return x
                     .Split(',')
                     .Select(x => int.Parse(x))
-                    .ToList();
-                return new Junction([splitOut[0], splitOut[1], splitOut[2]], null);
-            })];
+                    .ToArray();
+            }).ToList();
+        List<Junction> junctions = [];
+        for (int i = 0; i < junctionArrays.Count; i++)
+        {
+            junctions.Add(new Junction(i, junctionArrays[i]));
+        }
 
         // Calculate all the distances
         List<PointDistance> distances = [];
@@ -45,38 +49,166 @@ public class Day8
             for (int j = 0; j < junctions.Count; j++)
             {
                 if (i == j) continue;
-                distances.Add(new(junctions[i].Coordinates, junctions[j].Coordinates));
+                distances.Add(new(junctions[i].Coordinates, junctions[i].Id, junctions[j].Coordinates, junctions[j].Id));
             }
         }
-        distances = distances.DistinctBy(x => x.Distance).ToList();
+        distances = [.. distances.DistinctBy(x => x.Distance).OrderBy(x => x.Distance)]; // I think it's easier to just filter later, the calculation is cheap
 
-        return 0;
+        for (int i = 0; i < connectionCount; i++)
+        {
+            PointDistance currentClosest = distances[i];
+            Junction junctionA = junctions[currentClosest.PointAId];
+            Junction junctionB = junctions[currentClosest.PointBId];
+
+            junctionA.AddConnection(junctionB.Id, currentClosest.Distance);
+            junctionB.AddConnection(junctionA.Id, currentClosest.Distance);
+
+            Guid circuitID = Guid.NewGuid();
+            HashSet<int> circuitJunctions= [];
+            circuitJunctions.UnionWith(junctionA.ConnectedJunctions.Select(x => x.Id));
+            circuitJunctions.UnionWith(junctionB.ConnectedJunctions.Select(x => x.Id));
+            int addedJunctionCount = 0;
+            do
+            {
+                int startingJunctions = circuitJunctions.Count;
+                var loopJunctions = circuitJunctions.ToList();
+                foreach (var circuitJunction in loopJunctions)
+                {
+                    circuitJunctions.UnionWith(junctions[circuitJunction].ConnectedJunctions.Select(x => x.Id));
+                }
+                addedJunctionCount = circuitJunctions.Count - startingJunctions;
+            }
+            while (addedJunctionCount > 0);
+
+            foreach (var id in circuitJunctions)
+            {
+                junctions[id].CircuitId = circuitID;
+            }
+        }
+
+        var circuitProduct = junctions
+            .GroupBy(x => x.CircuitId)
+            .Select(g => new
+            {
+                Count = g.Count()
+            })
+            .Select(x => x.Count)
+            .OrderDescending()
+            .Take(3)
+            .Aggregate(1, (acc, n) => acc * n);
+
+        return circuitProduct;
     }
-    public static UInt128 Day8Part2Answer()
+    public static ulong Day8Part2Answer()
     {
-        return 0;
-    }
+        /** Initial Thoughts:
+         * I'm failing to see the massive complication here but I'm probably missing something.
+         * I'm gonna just update my iterator to be a do while loop instead of looping 1000
+         * times.
+         * 
+         * Once the circuit ID is the same in the collection, I will know what to check.
+         */
 
+        /** Final Thoughts:
+         *  Part 2 didn't really represent much difficulty to me, probably due to how I
+         *  setup part 1 with my classes. This is a good example of how the foundational
+         *  architecture of systems being designed for expansability and flexibility is
+         *  important.
+         *  
+         *  My only issue was the result was larger than I anticipated and I got a
+         *  wraparound until I made it uint64.
+         */
+
+        // Take in the input
+        var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Day8", "input.txt");
+        var junctionArrays = File.ReadAllLines(filePath).Select(x =>
+        {
+            return x
+                .Split(',')
+                .Select(x => int.Parse(x))
+                .ToArray();
+        }).ToList();
+        List<Junction> junctions = [];
+        for (int i = 0; i < junctionArrays.Count; i++)
+        {
+            junctions.Add(new Junction(i, junctionArrays[i]));
+        }
+
+        // Calculate all the distances
+        List<PointDistance> distances = [];
+        for (int i = 0; i < junctions.Count; i++)
+        {
+            for (int j = 0; j < junctions.Count; j++)
+            {
+                if (i == j) continue;
+                distances.Add(new(junctions[i].Coordinates, junctions[i].Id, junctions[j].Coordinates, junctions[j].Id));
+            }
+        }
+        distances = [.. distances.DistinctBy(x => x.Distance).OrderBy(x => x.Distance)]; // I think it's easier to just filter later, the calculation is cheap
+
+        int distanceIndex = 0;
+        int lastJunctionAXCoords = 0;
+        int lastJunctionBXCoords = 0;
+        do
+        {
+            PointDistance currentClosest = distances[distanceIndex];
+            distanceIndex++;
+            Junction junctionA = junctions[currentClosest.PointAId];
+            Junction junctionB = junctions[currentClosest.PointBId];
+
+            lastJunctionAXCoords = junctionA.Coordinates[0];
+            lastJunctionBXCoords = junctionB.Coordinates[0];
+
+            junctionA.AddConnection(junctionB.Id, currentClosest.Distance);
+            junctionB.AddConnection(junctionA.Id, currentClosest.Distance);
+
+            Guid circuitID = Guid.NewGuid();
+            HashSet<int> circuitJunctions = [];
+            circuitJunctions.UnionWith(junctionA.ConnectedJunctions.Select(x => x.Id));
+            circuitJunctions.UnionWith(junctionB.ConnectedJunctions.Select(x => x.Id));
+            int addedJunctionCount = 0;
+            do
+            {
+                int startingJunctions = circuitJunctions.Count;
+                var loopJunctions = circuitJunctions.ToList();
+                foreach (var circuitJunction in loopJunctions)
+                {
+                    circuitJunctions.UnionWith(junctions[circuitJunction].ConnectedJunctions.Select(x => x.Id));
+                }
+                addedJunctionCount = circuitJunctions.Count - startingJunctions;
+            }
+            while (addedJunctionCount > 0);
+
+            foreach (var id in circuitJunctions)
+            {
+                junctions[id].CircuitId = circuitID;
+            }
+        }
+        while (junctions.Select(x => x.CircuitId).Distinct().Count() > 1);
+
+        return (ulong)lastJunctionAXCoords * (ulong)lastJunctionBXCoords;
+    }
 }
 
 
-public class Junction(int[] coordinates, Guid? circuitId = null)
+public class Junction(int id, int[] coordinates)
 {
+    public int Id { get; set; } = id;
     public int[] Coordinates { get; } = coordinates;
-    public Guid? CircuitId { get; set; } = circuitId;
-    public List<(int[] Coordinates, double Distance)> ConnectedJunctions { get; set; } = [];
+    public Guid? CircuitId { get; set; } = Guid.NewGuid();
+    public List<(int Id, double Distance)> ConnectedJunctions { get; set; } = [];
 
-    public void AddConnection(int[] coordinates, double distance)
+    public void AddConnection(int id, double distance)
     {
-        ConnectedJunctions.Add((coordinates, distance));
+        ConnectedJunctions.Add((id, distance));
     }
 }
 
-public class PointDistance(int[] pointA, int[] pointB)
+public class PointDistance(int[] pointA, int pointAId, int[] pointB, int pointBId)
 {
-    public int[] PointA { get; } = pointA;
-    public int[] PointB { get; } = pointB;
-    public double? Distance { get; } = CalculateDistance(pointA, pointB);
+    public int PointAId { get; set; } = pointAId;
+    public int PointBId { get; set; } = pointBId;
+    public double Distance { get; } = CalculateDistance(pointA, pointB);
 
     // Rather than calculate the distance myself I'll lean on others:
     // Removed guard clauses and changed to int for my problem
